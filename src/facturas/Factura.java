2eb2214801efcd81f7e9fd2160c6e45525162afe
jmtello <jmtello@indra.es>
2017-09-20 12:57:20 +0200

    Merge drop octubre a sit1

--- src/facturas/Factura.java
+++ src/facturas/Factura.java
@@ -1,12 +1,27 @@
 package facturas;
 
+import java.util.Date;
+
 /**
  * FACTURAS 
- * @author proveedor2
+ * @author jmtello
  *
  */
 public class Factura {
 
+	private Date fecha;
+	
+	public String sayHello()
+	{
+		return "Hello";
+	}
+	
+	public void printDate() {
+		
+		System.out.println(new Date());
+		
+	}
+	
 	public static void proveedor2()
 	{
 		//CAMBIO PROVEEDOR2
@@ -14,3 +29,5 @@ public class Factura {
 		//Otro cambio de ialcazar, proveeedor 2
 	}
 }
+	
+
