b67b212b9ad44f5b9c6423258c493f4b1fad1c24
jmtello <jmtello@indra.es>
2017-10-04 12:06:17 +0200

    Subida cambio3 y merge conflictos

--- src/facturas/Factura.java
+++ src/facturas/Factura.java
@@ -7,29 +7,13 @@ package facturas;
  */
 public class Factura {
 
-	//Linea 11
+	// Cambio 1 del 04/10
+
+	// Cambio 1 proveedor2.2
+
+	// Cambio 2 del 04/10
 	
-	//Cambio BB
-	
-	//Cambio DROP OCTUBRE
-	
-	//Cambio Martes
-	
-	//Cambio 03/10/2017
-	
-	//Segundo cambio 03/10
-	
-	//Tercer cambio 03/10
-	
-	//Cuarto cambio 03/10
-	
-	//Quinto cambio
-	
-	//Cambio 1  del 04/10
-	
-	//Cambio 1 proveedor2.2
-	
-	//Cambio 2  del 04/10
+	//Cambio 3 proveedor2
 	
 	//Cambio 3  del 04/10
 	
